524f962c2500e53f81d2974884bc36caa27fa288
Yasmina1992 <yasminkhan2014.yk@gmail.com>
2025-03-16 15:23:27 +0530

    initial phase of JWT authentication

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, status, HTTPException, Depends
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from typing import Annotated
+import auth
+
+app = FastAPI()
+app.include_router(auth.router)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Athentication Failed")
+    return {"User": user}
